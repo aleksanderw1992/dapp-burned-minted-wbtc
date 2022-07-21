2d380215941895caae6171b9773c717c11f12b9b
Aleksander Wojcik <aleksander.w1992@gmail.com>
2022-07-22 01:19:07 +0200

    try fixing:
    index.js:1 Warning: Each child in a list should have a unique "key" prop.
    
    Check the render method of `TransactionList`
    
    with no effect

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -4,7 +4,7 @@ import classes from './Transaction.module.css';
 
 const Transaction = (props) => {
   return (
-    <li className={classes.transaction}>
+    <li className={classes.transaction} key={props.id}>
       Hash: <p><a href={"https://etherscan.io/tx/" + props.hash} target={"_blank"}  rel="noopener noreferrer">{props.hash}</a></p>
       From: <p>{props.from}</p>
       Time (en-GB): <p>{props.time}</p>
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -6,9 +6,10 @@ import classes from './TransactionsList.module.css';
 const TransactionList = (props) => {
   return (
     <ul className={classes['transactions-list']}>
-      <li className={classes.title}>{props.title}</li>
+      <li className={classes.title} key={props.title}>{props.title}</li>
       {props.transactions.map((transaction) => (
         <Transaction
+          id={transaction.id}
           hash={transaction.hash}
           from={transaction.from}
           time={transaction.time}
